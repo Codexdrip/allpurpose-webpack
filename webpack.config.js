2f52b2b429c9de2d54ab2dadca7005f0fa6c3106
CRFullStack <hobdy.chris02@gmail.com>
2019-10-13 18:41:50 -0400

    Updated webpack, images now show up in dist/images/[name].[ext]

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,14 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "/images/[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
